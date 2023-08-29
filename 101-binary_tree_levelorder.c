362106e459c87f1ce1919e51af8d836c8d7fae1c
Suedreamer <mulingesuzan88@gmail.com>
2023-08-29 06:00:04 -0700

    Level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,28 +1,56 @@
 #include "binary_trees.h"
-/**
- * binary_trees_ancestor - locate the lowest minimum ancestor
- * @first: pointer to the first node
- * @second: pointer to the second node
- * Return: the pointer to the L.M.A
- */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-		const binary_tree_t *second)
-{
-	binary_tree_t *a, *b;
 
-	if (!first || !second)
+/**
+ * binary_tree_levelorder - traverst a binary tree using level-order traverse
+ * @tree: tree to traverse
+ * @func: pointer to a function to call for each node
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	size_t level, maxlevel;
+
+	if (!tree || !func)
+		return;
+
+	maxlevel = binary_tree_height(tree) + 1;
+
+	for (level = 1; level <= maxlevel; level++)
+		btlo_helper(tree, func, level);
+}
+
+/**
+ * btlo_helper - goes through a binary tree using post-order traverse
+ * @tree: tree to traverse
+ * @func: pointer to a function to call for each node
+ * @level: the level of the tree to call func upon
+ */
+void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level)
+{
+	if (level == 1)
+		func(tree->n);
+	else
 	{
-		return (NULL);
+		btlo_helper(tree->left, func, level - 1);
+		btlo_helper(tree->right, func, level - 1);
 	}
-	if (first == second)
-	{
-		return ((binary_tree_t *)first);
-	}
-	a = first->parent;
-	b = second->parent;
-	if (first == a || !b || (!b->parent && a))
-		return (binary_trees_ancestor(first, a));
-	else if (b == second || !a || (!a->parent && b))
-		return (binary_trees_ancestor(b, second));
-	return (binary_trees_ancestor(b, a));
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: tree to measure the height of
+ *
+ * Return: height of the tree
+ *         0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
